226ff7f89d321076b947c7cab5fa44024dd3317a
Dev-C137 <dev.91096@gmail.com>
2018-09-24 19:41:35 +0530

    Added Headers

--- get-images.py
+++ get-images.py
@@ -2,15 +2,22 @@ import requests
 import urllib.request
 
 api_key = '4072f60bf9e8a68eb2ac5dfc08cd9bc4'
-page_counter = 1
 photo_url = "https://farm{}.staticflickr.com/{}/{}_{}.jpg"
 
 
 def download_img(img_url, img_id):
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)
         file_name = 'dataset/' + img_id + '.jpg'
         print('Downloading ' + img_id + '.jpg....')
         urllib.request.urlretrieve(img_url, file_name)
 
+# TODO: Implement Error handling
+# TODO: Put everything in functions
+
+
+page_counter = 1
 
 while True:
         response = requests.get('https://api.flickr.com/services/rest/', params={
@@ -18,7 +25,7 @@ while True:
                 'format': 'json',
                 'nojsoncallback': 1,
                 'api_key': api_key,
-                'tags': 'landscape',
+                'tags': 'landscape,trees,mountains',
                 'tag_mode': 'all',
                 'page': page_counter,
             }).json()
